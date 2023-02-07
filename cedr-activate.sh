c5fdb306bf302938a3f6c85445b03bc7c99de6d0
Haxer <115757568+rainestorme@users.noreply.github.com>
2023-02-07 11:48:50 -0600

    Create cedr-activate.sh

---
+++ cedr-activate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# cedr - the ChromeOS Enrolled Desktop Runtime
+# Licensed under the MIT License (https://github.com/rainestorme/cedr/blob/main/LICENSE)
+
+# Get the passed parameters
+desktop=$1
+
+pushd /home/chronos/usbdrv
+
+# Add host-bin to $PATH
+export PATH="/home/chronos/usbdrv/crouton/host-bin:$PATH"
+
+# Enter chroot
+$1
+
+# Exit
+popd
