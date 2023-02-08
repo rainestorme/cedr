d1a0ed0b0bf81e7bfd25cb6b4c3b850504bf4e7f
Haxer <115757568+rainestorme@users.noreply.github.com>
2023-02-08 09:11:34 -0600

    Yet more shellcheck

--- cedr-mount.sh
+++ cedr-mount.sh
@@ -5,13 +5,13 @@
 # Get the passed parameters
 drive=$1
 
-if [ ! -b $drive ]; then
+if [ ! -b "$drive" ]; then
   echo "[cedr] Error: $drive is not a block device"
   exit 1
 fi
 
-umount $drive
+umount "$drive"
 mkdir -p /home/chronos/usbdrv
-mount -o exec,suid,dev,symfollow $drive /home/chronos/usbdrv
+mount -o exec,suid,dev,symfollow "$drive" /home/chronos/usbdrv
 
 /home/chronos/usbdrv/cedr-activate.sh
