9ce8efa424fe5b70cd38419ec29ea0988f42dd35
Haxer <115757568+rainestorme@users.noreply.github.com>
2023-02-08 09:13:02 -0600

    Make output more verbose, fix shellcheck violation

--- cedr-umount.sh
+++ cedr-umount.sh
@@ -2,7 +2,7 @@
 # cedr - the ChromeOS Enrolled Desktop Runtime
 # Licensed under the MIT License (https://github.com/rainestorme/cedr/blob/main/LICENSE)
 
-cd /home/chronos | exit
-umount /home/chronos/usbdrv | exit
+cd /home/chronos || exit
+umount /home/chronos/usbdrv || echo "[cedr] Failed to umount /home/chronos/usbdrv!" && exit
 
 echo "[cedr] Unmounted /home/chronos/usbdrv successfully."
