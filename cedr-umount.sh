798e993560ac5a7696ed8160d39631b0869102c4
Haxer <115757568+rainestorme@users.noreply.github.com>
2023-02-08 08:28:00 -0600

    Create cedr-umount.sh

---
+++ cedr-umount.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# cedr - the ChromeOS Enrolled Desktop Runtime
+# Licensed under the MIT License (https://github.com/rainestorme/cedr/blob/main/LICENSE)
+
+cd /home/chronos | exit
+umount /home/chronos/usbdrv | exit
+
+echo "[cedr] Unmounted /home/chronos/usbdrv successfully."
